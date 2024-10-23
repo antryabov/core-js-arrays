b0aaaf261ea518ed41fb380d2540c58234dcf4e9
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-10-23 18:04:01 +0300

    feat: solve 13 problems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -132,8 +132,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (!arr.length) return 0;
+  const sum = arr.reduce((acc, el) => acc + el, 0);
+  const answer = sum / arr.length;
+
+  return Number.isInteger(answer) ? answer : +answer.toFixed(2);
 }
 
 /**

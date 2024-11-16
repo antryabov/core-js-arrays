ab82cec96eb39dd47e1d90a92733735d55a07cbe
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-11-16 20:23:06 +0300

    feat: solve 38 problems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -307,7 +307,7 @@ function flattenArray(nestedArray) {
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-  return arr.map(childrenSelector).flat();
+  return arr.flatMap(childrenSelector);
 }
 
 /**

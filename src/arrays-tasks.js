177d6aa8c59b9552add5400ff7d8cf5b44a015fb
Anton Ryabov <antonryabovtmb@yandex.ru>
2024-11-20 19:01:50 +0300

    feat: solve 47 problems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -608,8 +608,11 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const newArr = n < 0 ? arr.slice(-n) : arr.slice(0, n + 1);
+  const negativeN = n < 0 ? arr.slice(0, -n) : arr.slice(n + 1);
+
+  return n < 0 ? [...newArr, ...negativeN] : [...negativeN, ...newArr];
 }
 
 /**
